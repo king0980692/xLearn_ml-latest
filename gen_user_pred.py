9547ec796cf3e5ebc374ba0fe8bbda45b5f2c7ee
Leon <king0980692@gmail.com>
2021-10-25 18:28:44 +0800

    Add notebook

--- gen_user_pred.py
+++ gen_user_pred.py
@@ -17,24 +17,25 @@ last_user = ""
 with open(args.score_file,'r') as o:   
     first_flag = True
     #total_len = buf_count_newlines_gen(args.truth_file)
-    total_len = 7661096275
+    total_len = 7642697175
+    total_len = 242049
     idx = 0
     with open(args.truth_file, "r+b") as f:
         map_file = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
-        pbar = tqdm(iter(map_file.readline, b""),total=total_len)
-        for line in pbar:
+        pbar = tqdm(total=total_len)
+        for line in iter(map_file.readline, b""):
             score = o.readline().rstrip()
+            idx += 1
+            if float(score) < 4:
+                continue
             #if idx == 10:
             #    break
-            #idx += 1
 
             line = line.decode()    
             user,item = line.rstrip().split(" ")
             user = user.split(':')[0]
             item = item.split(':')[0]
             #print(user,item)
-            if float(score) < 4:
-                continue
             
             if first_flag:
                 last_user = user 
@@ -45,22 +46,21 @@ with open(args.score_file,'r') as o:
                 user_pred_dict[last_user] = user_pred_dict[last_user][:10]
                 gc.collect()
                 last_user = user
+                print(f"{user} done at line:{idx} !!")
+                pbar.update(1)
+                with open("./user_pred_dict.pkl",'wb') as p:
+                    pickle.dump(user_pred_dict,p)        
 
             if user not in user_pred_dict:
                 user_pred_dict[user] = [] 
             user_pred_dict[user].append((item,score))
 
+
 # the last user 
 user_pred_dict[user].sort(key=lambda x:x[1],reverse=True)
 user_pred_dict[user] = user_pred_dict[last_user][:10]
 
-for k in user_pred_dict:
-    print(k, user_pred_dict[k])
-    #print(len(user_pred_dict[k]))
-    #break
 
 
-with open("./user_pred_dict.pkl",'wb') as p:
-    pickle.dump(user_pred_dict,p)        
 
 
