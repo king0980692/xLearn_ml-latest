3e319f09d981df625a9368a2335beed2330660bf
Leon <king0980692@gmail.com>
2021-10-31 13:09:10 +0800

    Update

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-#mkidr ./data
-#wget https://files.grouplens.org/datasets/movielens/ml-100k.zip -P ./data
-#unzip ./data/ml-100k.zip -d ./data
+mkidr ./data
+wget https://files.grouplens.org/datasets/movielens/ml-100k.zip -P ./data
+unzip ./data/ml-100k.zip -d ./data
 
 python3 ./encoderder/encoderder.py -c ./test.json
 
 python3 train_predict.py
 
-poetry run python3 gen_user_pred.py --score_file ./result/output.txt --truth_file ./exp/ml.test
+poetry run python3 gen_user_pred.py --score_file ./result/output.txt --truth_file ./exp/ml.test.all_pair
 
 python3 eval.py
